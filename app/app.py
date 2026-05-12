7588f217d5e53c822f8889cbcf589656e6c8ecb8
Vivek Anand <78247712+viveka1302@users.noreply.github.com>
2026-05-12 18:17:20 +0530

    db set up

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from .schemas import PostCreate
+from .db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app= FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app= FastAPI(lifespan=lifespan)
 
 # @app.get("/hello-world")
 # def hello_world():
